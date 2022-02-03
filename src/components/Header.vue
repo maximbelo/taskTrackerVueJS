a64b0268dfa2b5a83ad1537126a8f5f3ed5288a4
maximbelo <mbelomestnov@hotmail.ca>
2022-02-03 14:46:33 -0500

    Added a btn component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,22 @@
 <template>
   <header>
-    <h1>Task Tracker</h1>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
   </header>
 </template>
 
 <script>
+import Button from "./Button";
+
 export default {
   name: "Header",
+  // Props can be defined as an array or object -> If added as an object we define the type and we can also define a default value
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
 };
 </script>
 
